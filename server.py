f317302428bc50fb8c0bf7698f2e437bdcd561ad
Paola Socorro <pao.socorro@gmail.com>
2015-11-14 01:31:53 -0800

    moved files around

--- server.py
+++ server.py
@@ -1,35 +1,14 @@
-from jinja2 import StrictUndefined
-
-from flask import Flask, render_template, redirect, request,url_for, flash, session, jsonify,abort
-from flask_debugtoolbar import DebugToolbarExtension
-import os
-
-
-SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
-
-app.secret_key = SECRET_KEY
-
-
-app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
 def index():
-    """Homepage."""
-
-
+    """Show index page."""
     return render_template("index.html")
 
 
 
 if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 5000))
-    # DebugToolbarExtension(app)
-
-
-    
-    DEBUG = "NO_DEBUG" not in os.environ
-    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
-    # app.run()
+    app.run(debug=False)
